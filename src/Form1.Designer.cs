def3ffeb88568e6e44ad8d4a14e7baa7a6556acd
bharatmane <Dhami2480!>
2019-12-24 12:06:26 +0530

    Added initial files

---
+++ src/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace ImageStitch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lstImages = new System.Windows.Forms.ListBox();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnStitch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(283, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(769, 608);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lstImages
+            // 
+            this.lstImages.FormattingEnabled = true;
+            this.lstImages.Location = new System.Drawing.Point(12, 67);
+            this.lstImages.Name = "lstImages";
+            this.lstImages.Size = new System.Drawing.Size(265, 550);
+            this.lstImages.TabIndex = 1;
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Location = new System.Drawing.Point(12, 38);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.Size = new System.Drawing.Size(227, 20);
+            this.txtFolder.TabIndex = 2;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(245, 38);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(32, 23);
+            this.btnBrowse.TabIndex = 3;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Select folder to pick the images";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(280, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Final Image";
+            // 
+            // btnStitch
+            // 
+            this.btnStitch.Location = new System.Drawing.Point(12, 623);
+            this.btnStitch.Name = "btnStitch";
+            this.btnStitch.Size = new System.Drawing.Size(265, 23);
+            this.btnStitch.TabIndex = 6;
+            this.btnStitch.Text = "STITCH";
+            this.btnStitch.UseVisualStyleBackColor = true;
+            this.btnStitch.Click += new System.EventHandler(this.btnStitch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1064, 665);
+            this.Controls.Add(this.btnStitch);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtFolder);
+            this.Controls.Add(this.lstImages);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Image Stitcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox lstImages;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStitch;
+    }
+}
+
---
+++ src/Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+namespace ImageStitch
+{
+   
+    public partial class Form1 : Form
+    {
+        int fullImageWidth = 0;
+        int fullImageHeight = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    txtFolder.Text = fbd.SelectedPath;
+                    string[] files = Directory.GetFiles(fbd.SelectedPath);
+
+                    lstImages.DataSource = files;
+                }
+            }
+        }
+
+        private void btnStitch_Click(object sender, EventArgs e)
+        {
+            List<int> columns;
+            List<int> rows;
+            int finalImageHeight = 0;
+            int finalImageWidth= 0;
+            columns = _GetNumberOfColumns().ToList();
+            rows = _GetNumberOfRows().ToList();
+            columns.Sort();
+            rows.Sort();
+            finalImageHeight = _GetFinalImageHeight(rows,columns);
+            finalImageWidth = _GetFinalImageWidth(rows, columns);
+
+            string finalImagePath = txtFolder.Text + @"\StitchedImage.jpg";
+            Bitmap finalImage = _StitchImages(rows,columns,finalImageHeight,finalImageWidth);
+            finalImage.Save(finalImagePath);
+            finalImage.Dispose();
+            pictureBox1.Image = Image.FromFile(finalImagePath);
+        }
+
+        private Bitmap _StitchImages(IEnumerable<int> rows, IEnumerable<int> columns, int finalImageHeight, int finalImageWidth)
+        {
+            Bitmap finalImage = null;
+            try
+            {
+                finalImage = new Bitmap(finalImageWidth, finalImageHeight);
+                using (Graphics g = Graphics.FromImage(finalImage))
+                {
+                    g.Clear(Color.Black);
+                    int offsetLeft = 0;
+                    int offsetTop = 0;
+                    foreach (var row in rows)
+                    {
+                        int maxColumnHeight = 0;
+                        offsetLeft = 0;
+                        foreach (var column in columns)
+                        {
+                            string path = txtFolder.Text + @"\" + column.ToString() + "_" + row.ToString() + ".jpg";
+                            if (File.Exists(path))
+                            {
+                                Bitmap image = new Bitmap(path);
+                                g.DrawImage(image, new Rectangle(offsetLeft, offsetTop, image.Width, image.Height));
+                                offsetLeft += image.Width;
+                                maxColumnHeight = Math.Max(maxColumnHeight, image.Height);
+                                image.Dispose();
+                            }
+                        }
+                        offsetTop += maxColumnHeight;
+                    }
+                }
+                return finalImage;
+            }
+            catch (Exception ex)
+            {
+                if (finalImage != null)
+                    finalImage.Dispose();
+                throw ex;
+            }
+            
+        }
+
+        private int _GetFinalImageWidth(IEnumerable<int> rows, IEnumerable<int> columns)
+        {
+            int width = 0;
+            foreach (var column in columns)
+            {
+                int maxRowWidth = 0;
+                foreach(var row in rows)
+                {
+                    string path = txtFolder.Text + @"\" + column.ToString() + "_" + row.ToString() + ".jpg";
+                    if (File.Exists(path))
+                    {
+                        Bitmap image = new Bitmap(path);
+                        maxRowWidth = Math.Max(maxRowWidth, image.Width);
+                        image.Dispose();
+                    }
+                }
+                width += maxRowWidth;
+            }
+            return width;
+        }
+
+        private int _GetFinalImageHeight(IEnumerable<int> rows, IEnumerable<int> columns)
+        {
+            int height = 0;
+            foreach (var row in rows)
+            {
+                int maxColumnHeight = 0;
+                foreach (var column in columns)
+                {
+                    string path = txtFolder.Text + @"\" + column.ToString() + "_" + row.ToString() + ".jpg";
+                    if (File.Exists(path))
+                    {
+                        Bitmap image = new Bitmap(path);
+                        maxColumnHeight = Math.Max(maxColumnHeight, image.Height);
+                        image.Dispose();
+                    }
+                }
+                height += maxColumnHeight;
+            }
+            return height;
+        }
+
+        private IEnumerable<int> _GetNumberOfRows()
+        {
+            List<int> indexes = new List<int>();
+            foreach(var item in lstImages.Items)
+            {
+                FileInfo fileInfo = new FileInfo(item.ToString());
+                if(fileInfo.Name.IndexOf("_") > -1)
+                {
+                    int startIndex = fileInfo.Name.IndexOf("_") + 1;
+                    int length = fileInfo.Name.IndexOf(".") - startIndex;
+                    indexes.Add(Convert.ToInt16(fileInfo.Name.Substring(startIndex, length)));
+                }
+            }
+            return indexes.Distinct();
+        }
+
+        private IEnumerable<int> _GetNumberOfColumns()
+        {
+            List<int> indexes = new List<int>();
+            foreach (var item in lstImages.Items)
+            {
+                FileInfo fileInfo = new FileInfo(item.ToString());
+                if (fileInfo.Name.IndexOf("_") > -1)
+                {
+                    int startIndex = 0;
+                    int length = fileInfo.Name.IndexOf("_");
+                    indexes.Add(Convert.ToInt16(fileInfo.Name.Substring(startIndex, length)));
+                }
+            }
+            return indexes.Distinct();
+        }
+
+
+
+
+        //private void CombineImages(MyImage[,] images)
+        //{
+        //    //change the location to store the final image.
+
+
+
+        //    string finalImage = @"E:\Seite\asdsad.png";
+        //    int fullWidth = 0;
+        //    int fullheight= 0;
+        //    for (int i = 0; i < 1; i++)
+        //    {
+        //        int currentWidth = 0;
+        //        int maxHeightCurrentRow = 0;
+        //        for (int j = 2; j < 5; j++)
+        //        {
+        //            currentWidth += images[i, j].Width;
+        //            if(maxHeightCurrentRow < images[i, j].Height)
+        //            {
+        //                maxHeightCurrentRow = images[i, j].Height;
+        //            }
+        //        }
+        //        if(fullWidth < currentWidth)
+        //        {
+        //            fullWidth = currentWidth;
+        //        }
+        //        fullheight += maxHeightCurrentRow;
+        //    }
+        //    //create a bitmap to hold the combined image
+        //    System.Drawing.Bitmap finalImage1 = new System.Drawing.Bitmap(fullWidth, fullheight);
+
+        //    //get a graphics object from the image so we can draw on it
+        //    using (System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(finalImage1))
+        //    {
+        //        //set background color
+        //        g.Clear(System.Drawing.Color.Black);
+
+        //        //go through each image and draw it on the final image
+        //        int offset = 0;
+        //        for (int j = 2; j < 5; j++)
+        //        {
+        //            Image img = Image.FromFile(images[0, j].FullName);
+        //            g.DrawImage(img,
+        //              new System.Drawing.Rectangle(offset, 0, img.Width, img.Height));
+        //            offset += img.Width;
+        //        }
+        //    }
+
+        //    //Bitmap img3 = new Bitmap(fullWidth,fullheight);
+        //    //Graphics g = Graphics.FromImage(img3);
+        //    //g.Clear(SystemColors.AppWorkspace);
+        //    //int locationx = 0;
+        //    //int locationy = 0;
+        //    //for (int i = 0; i < 1; i++)
+        //    //{
+        //    //    locationx = 0;
+        //    //    for (int j = 0; j < 2; j++)
+        //    //    {
+        //    //        Image img = Image.FromFile(images[i,j].FullName);
+        //    //        g.DrawImage(img, new Point(locationx, locationy));
+        //    //        locationx += images[i, j].Width;
+        //    //        g.DrawImage(img, new Point(locationx, locationy));
+        //    //        img.Dispose();
+        //    //    }
+        //    //    locationy += 256;
+        //    //}
+
+        //    //g.Dispose();
+        //    finalImage1.Save(finalImage, System.Drawing.Imaging.ImageFormat.Png);
+        //    finalImage1.Dispose();
+        //    pictureBox1.Image = Image.FromFile(finalImage);
+        //}
+    }
+}
